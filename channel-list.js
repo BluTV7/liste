3950e34352645635091f14825689bfaed8f73f0f
aykut-iptv <aykut.erduran.pc@gmail.com>
2026-03-16 11:20:47 +0300

    Update channel-list.js

--- channel-list.js
+++ channel-list.js
@@ -22,6 +22,18 @@ stream:"https://ciner.daioncdn.net/showtv/showtv_1080p.m3u8?app=showtv_web&ce=3"
 name:"Star TV",
 logo:"https://drive.google.com/thumbnail?id=1rQDPzvVLmq4NB82ppMXVYXfdGG-kwSlL&sz=w1000",
 stream:"https://dogus.daioncdn.net/startv/startv_720p.m3u8?app=startv_web&ce=3"
-}  
+},  
+
+{
+name:"ATV",
+logo:"https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/Atv_logo_2010.svg/3840px-Atv_logo_2010.svg.png",
+stream:"https://rnttwmjcin.turknet.ercdn.net/lcpmvefbyo/atv/atv.m3u8"
+},
+
+{
+name:"NOW",
+logo:"https://upload.wikimedia.org/wikipedia/commons/thumb/d/db/NOW_TV_%28Turkey%29_wordmark-red.svg/960px-NOW_TV_%28Turkey%29_wordmark-red.svg.png",
+stream:"https://yakala.atakann.workers.dev/?url=https://www.nowtv.com.tr/canli-yayin&ext=m3u8"
+},
   
 ];
