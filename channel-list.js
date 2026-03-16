7e47e86b579bf9b9a018f21b1fd1d9430afabae1
aykut-iptv <aykut.erduran.pc@gmail.com>
2026-03-16 10:56:14 +0300

    Update channel-list.js

--- channel-list.js
+++ channel-list.js
@@ -9,7 +9,7 @@ stream:"https://tv-trt1.medya.trt.com.tr/master_1080.m3u8"
 {
 name:"Kanal D",
 logo:"https://upload.wikimedia.org/wikipedia/tr/archive/4/4e/20240406161353%21Kanal_D.png",
-stream:"https://demiroren.daioncdn.net/kanald/kanald.m3u8"
+stream:"https://demiroren.daioncdn.net/kanald/kanald.m3u8?app=kanald_web&ce=3"
 },
 
 {
