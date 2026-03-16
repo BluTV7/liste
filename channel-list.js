f5ee2e658559b39f4055bf048fe896a8b1b081c4
aykut-iptv <aykut.erduran.pc@gmail.com>
2026-03-16 11:38:33 +0300

    Update channel-list.js

--- channel-list.js
+++ channel-list.js
@@ -35,5 +35,41 @@ name:"NOW",
 logo:"https://upload.wikimedia.org/wikipedia/commons/thumb/d/db/NOW_TV_%28Turkey%29_wordmark-red.svg/960px-NOW_TV_%28Turkey%29_wordmark-red.svg.png",
 stream:"https://yakala.atakann.workers.dev/?url=https://www.nowtv.com.tr/canli-yayin&ext=m3u8"
 },
+
+{
+name:"TV8",
+logo:"https://upload.wikimedia.org/wikipedia/tr/6/68/Tv8_Yeni_Logo.png",
+stream:"https://tv8.daioncdn.net/tv8/tv8_1080p.m3u8?app=tv8_web&ce=3"
+},
+
+{
+name:"360",
+logo:"https://static.wikia.nocookie.net/logopedia/images/3/37/360-tr.png/revision/latest?cb=20220108145523",
+stream:"https://turkmedya-live.ercdn.net/tv360/tv360_1080p.m3u8"
+},  
+
+{
+name:"CNBC-e",
+logo:"https://drive.google.com/uc?export=view&id=1MrNjdIU_AQbMHohSYHs-rAw9B6bTmJ2w",
+stream:"https://hnpsechtsc.turknet.ercdn.net/xpnvudnlsv/cnbc-e/cnbc-e.m3u8"
+},
+
+{
+name:"Bloomberg HT",
+logo:"https://public.flourish.studio/uploads/f4c8dc45-ee56-403e-a5be-225565ca3a9e.png",
+stream:"https://ciner.daioncdn.net/bloomberght/bloomberght.m3u8"
+},
+
+{
+name:"A Haber",
+logo:"https://upload.wikimedia.org/wikipedia/commons/7/7c/Ahaber_Logo.png",
+stream:"https://streams.uzunmuhalefet.com/stream/b4f7a5e9-fcae-4258-83eb-cc37b6a68696.m3u8"
+},
+
+{
+name:"TRT Haber",
+logo:"https://drive.google.com/uc?id=1d1FHiWWYXK-bWywpXp06rBw-VgFyoebl",
+stream:"http://tv-trthaber.medya.trt.com.tr/master_1080.m3u8"
+},
   
 ];
