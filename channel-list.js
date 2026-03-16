a6b5407e86cea969731b0f37d50c37121c00a741
aykut-iptv <aykut.erduran.pc@gmail.com>
2026-03-16 09:36:38 +0300

    Update channel-list.js

--- channel-list.js
+++ channel-list.js
@@ -1,1 +1,15 @@
+const channelData = [
 
+{
+name:"TRT 1",
+logo:"https://r2.thesportsdb.com/images/media/channel/logo/42s3t41616613172.png",
+stream:"https://tv-trt1.medya.trt.com.tr/master_1080.m3u8"
+},
+
+{
+name:"Kanal D",
+logo:"https://upload.wikimedia.org/wikipedia/tr/archive/4/4e/20240406161353%21Kanal_D.png",
+stream:"https://demiroren.daioncdn.net/kanald/kanald.m3u8"
+}
+
+];
